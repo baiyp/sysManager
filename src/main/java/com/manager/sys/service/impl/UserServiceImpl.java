bc8674263531c1e2f6715bf9f22076a17c47d0a1
baiyp <66312288@qq.com>
2016-04-07 07:15:46 +0800

    shiro

--- src/main/java/com/manager/sys/service/impl/UserServiceImpl.java
+++ src/main/java/com/manager/sys/service/impl/UserServiceImpl.java
@@ -1,13 +1,10 @@
 package com.manager.sys.service.impl;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import org.apache.shiro.authc.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
- 
+
 import com.manager.sys.dao.UserDao;
 import com.manager.sys.model.User;
 import com.manager.sys.service.UserService;
