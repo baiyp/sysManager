476886f8748cbba6f18506c8715660fedf7b208b
baiyp <66312288@qq.com>
2016-04-11 07:11:21 +0800

    listener

---
+++ src/main/java/com/manager/common/listener/SystemSessionListener.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.manager.common.listener;
+
+import org.apache.shiro.session.Session;
+import org.apache.shiro.session.SessionListener;
+
+/**
+ * @author baiyp
+ *
+ */
+public class SystemSessionListener implements SessionListener {
+
+
+	@Override
+	public void onStart(Session session) { 
+		System.out.println("onStart--------------------------------------------------"); 
+	}
+
+
+	@Override
+	public void onStop(Session session) {
+		System.out.println("onStop---------------------------------------------------");
+	}
+
+
+	@Override
+	public void onExpiration(Session session) {
+		System.out.println("onExpiration----------------------------------------------"); 
+	}
+
+
+
+}
